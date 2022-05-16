82e0651be5c6bada3246bc6651041e11273702db
amank876655 <amank876655@gmail.com>
2022-05-17 04:50:09 +0530

    error in pubspec.yaml

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,15 +6,10 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-          child: Text(
-        "Login Page",
-        style: TextStyle(
-          fontSize: 60,
-          color: Colors.red,
-          fontWeight: FontWeight.bold,
-        ),
-      )),
-    );
+        child: Column(
+      children: [
+        Image.asset("assets/images/login_image.png"),
+      ],
+    ));
   }
 }
