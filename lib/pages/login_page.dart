a541200d10f1889c4d0a0141a0998c67527ebd92
amank876655 <amank876655@gmail.com>
2022-05-17 10:58:59 +0530

    day4 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,10 +6,47 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
+        color: Colors.white,
         child: Column(
-      children: [
-        Image.asset("assets/images/login_image.png"),
-      ],
-    ));
+          children: [
+            Image.asset(
+              "assets/image/login_image.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Enter username", labelText: "username"),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "Enter password", labelText: "password"),
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+                ElevatedButton(
+                  child: Text("Login"),
+                  style: TextButton.styleFrom(),
+                  onPressed: () {
+                    print("Hello Aman");
+                  },
+                )
+              ],
+            ),
+            Text(
+              "Welcome to flutter",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            )
+          ],
+        ));
   }
 }
